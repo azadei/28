5ec42b3b9cd7d300893b17adb4e00eb70b09bf70
azadei <azadei@code.edu.az>
2023-07-17 15:17:45 +0400

    1

---
+++ 28/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        string[] items = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+
+        
+        var arrangedList = items.OrderBy(item => item.Length)
+                                .ThenBy(item => item);
+
+       
+        Console.WriteLine("Here is the arranged list:");
+        foreach (var item in arrangedList)
+        {
+            Console.WriteLine(item);
+        }
+
+    }
+}
